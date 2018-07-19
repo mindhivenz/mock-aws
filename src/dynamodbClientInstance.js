d4661fd6a53d4e1f751deb16be4b97cea12bdee6
damon <damon@mahuika.co.nz>
2018-07-19 15:15:21 -0400

    Install DynamoDB in dynamodbClientInstance

--- src/dynamodbClientInstance.js
+++ src/dynamodbClientInstance.js
@@ -5,6 +5,7 @@ import DynamoDb from 'aws-sdk/clients/dynamodb'
 import { SINGLE_INSTANCE_PORT } from './dynamodbConst'
 import { checkPortInUse, freePort } from './ports'
 import startDynamodbLocal from './startDynamodbLocal'
+import dynamodbLocal from 'dynamodb-localhost'
 
 
 const clientFactory = port =>
@@ -20,10 +21,16 @@ const instancePromise = new Promise(async (resolve, reject) => {
       console.log(`Using existing dynamodb-local instance on ${SINGLE_INSTANCE_PORT}`)
       resolve(clientFactory(SINGLE_INSTANCE_PORT))
     } else {
-      const newPort = await freePort()
-      console.log(`Starting new dynamodb-local instance on ${newPort}`)
-      startDynamodbLocal(newPort)
-      resolve(clientFactory(newPort))
+      dynamodbLocal.install(async () => {
+        try {
+          const newPort = await freePort()
+          console.log(`Starting new dynamodb-local instance on ${newPort}`)
+          startDynamodbLocal(newPort)
+          resolve(clientFactory(newPort))
+        } catch (e) {
+          reject(e)
+        }
+      })
     }
   } catch (e) {
     reject(e)
